57631abbf76c2f97d7b1f833177bdff7d733d9f0
Vladislav <vladislavhacker111@gmail.com>
2020-10-22 02:09:45 +0300

    taskB fix

--- task_B.cpp
+++ task_B.cpp
@@ -1,76 +1,43 @@
-#include <cstdint>
-#include <string>
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cstdint>
 
-std::vector <int64_t> getOddVector (const std::string &s) {
-    
+std::vector <int64_t> getManakerVector (const std::string &s, bool flag) {
     std::vector <int64_t> v(s.size(), 0);
     size_t n = s.size();
     int64_t l = 0;
     int64_t r = -1;
-    
     for (int64_t i = 0; i < n; ++i) {
         int64_t k;
         if (i > r) {
-            k = 1;
+            k = static_cast<int>(flag);
         } else {
-            k = std::min(v[l + r - i], r - i + 1);
+            k = std::min(v[l + r - i + static_cast<int>(!flag)], r - i + 1);
         }
-        while (i + k < n && i - k >= 0 && s[i + k] == s[i - k]) {
+        while (
+                i + k < n && i - k - static_cast<int>(!flag) >= 0 &&
+                s[i + k] == s[i - k - static_cast<int>(!flag)]
+        ) {
             ++k;
         }
         v[i] = k;
         if (i + k - 1 > r) {
-            l = i - k + 1;
+            l = i - k + static_cast<int>(flag);
             r = i + k - 1;
         }
     }
-    
     return v;
-    
-}
-
-std::vector <int64_t> getEvenVector (const std::string &s) {
-    
-    std::vector <int64_t> v(s.size(), 0);
-    size_t n = s.size();
-    int64_t l = 0;
-    int64_t r = -1;
-    
-    for (int64_t i = 0; i < n; ++i) {
-        int64_t k;
-        if (i > r) {
-            k = 0;
-        } else {
-            k = std::min(v[l + r - i + 1], r - i + 1);
-        }
-        while (i + k < n && i - k - 1 >= 0 && s[i + k] == s[i - k - 1]) {
-            ++k;
-        }
-        v[i] = k;
-        if (i + k - 1 > r) {
-            l = i - k;
-            r = i + k - 1;
-        }
-    }
-    
-    return v;
-    
 }
 
 size_t manakerAlgorithm (const std::string &s) {
-    
-    std::vector <int64_t> d1 = getOddVector(s);
-    std::vector <int64_t> d2 = getEvenVector(s);
+    std::vector <int64_t> d1 = getManakerVector(s, true);  // odd
+    std::vector <int64_t> d2 = getManakerVector(s, false); // even
     size_t result = 0;
-    
     for (size_t i = 0; i < s.size(); ++i) {
         result += d1[i] + d2[i];
     }
-    
     return result - s.size();
-    
 }
 
 void getInput (
@@ -88,11 +55,11 @@ void setOutput (
 }
 
 int main() {
-    
+
     std::string s;
     getInput(std::cin, s);
 
     setOutput(std::cout, manakerAlgorithm(s));
-    
     return 0;
+
 }
